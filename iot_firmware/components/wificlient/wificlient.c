179948e6bf34ca43a119923eac54b86a8d5f3029
jun fujita <716663+jfcamel@users.noreply.github.com>
2021-08-22 16:55:13 +0900

    Refactoring event handlers & format code

--- iot_firmware/components/wificlient/wificlient.c
+++ iot_firmware/components/wificlient/wificlient.c
@@ -19,31 +19,41 @@ static const char *TAG = "wificlient";
 static void smartconfig_task(void *parm);
 static void wifi_event_handler(void* arg, esp_event_base_t event_base,
                                int32_t event_id, void* event_data);
+static void ip_event_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data);
+static void smart_config_event_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data);
 static const int CONNECTED_BIT = BIT0;
 static const int ESPTOUCH_DONE_BIT = BIT1;
 
 esp_err_t wifi_client_init(void)
 {
   ESP_LOGI(TAG, "start initializing.");
-  ESP_ERROR_CHECK( nvs_flash_init() );
-  ESP_ERROR_CHECK( esp_netif_init() );
+  ESP_ERROR_CHECK(nvs_flash_init());
+  ESP_ERROR_CHECK(esp_netif_init());
   s_wifi_client_event_group = xEventGroupCreate();
-  ESP_ERROR_CHECK( esp_event_loop_create_default() );
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
   esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
   assert(sta_netif);
 
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK( esp_wifi_init( &cfg ) );
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
   // WIFI EVENT
-  ESP_ERROR_CHECK( esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL) );
+  ESP_ERROR_CHECK(
+    esp_event_handler_register(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
   // IP EVENT
-  ESP_ERROR_CHECK( esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL) );
+  ESP_ERROR_CHECK(
+    esp_event_handler_register(
+      IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_handler, NULL));
   // Smart Config EVENT
-  ESP_ERROR_CHECK( esp_event_handler_register(SC_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL) );
+  ESP_ERROR_CHECK(
+    esp_event_handler_register(
+      SC_EVENT, ESP_EVENT_ANY_ID, &smart_config_event_handler, NULL));
 
-  ESP_ERROR_CHECK( esp_wifi_set_mode( WIFI_MODE_STA ) );
-  ESP_ERROR_CHECK( esp_wifi_start() );
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_start());
 
   ESP_LOGI(TAG, "initialized.");
   return ESP_OK;
@@ -62,14 +72,16 @@ esp_err_t wifi_client_wait_for_connected(TickType_t xTicksToWait)
 static void smartconfig_task(void *parm)
 {
   EventBits_t uxBits;
-  ESP_ERROR_CHECK( esp_smartconfig_set_type(SC_TYPE_ESPTOUCH) );
-  /* ESP_ERROR_CHECK( esp_smartconfig_set_type(SC_TYPE_AIRKISS) ); */
-  /* ESP_ERROR_CHECK( esp_smartconfig_set_type(SC_TYPE_ESPTOUCH_AIRKISS) ); */
-  /* ESP_ERROR_CHECK( esp_smartconfig_set_type(SC_TYPE_ESPTOUCH_V2) ); */
+  ESP_ERROR_CHECK(esp_smartconfig_set_type(SC_TYPE_ESPTOUCH));
+  /* ESP_ERROR_CHECK(esp_smartconfig_set_type(SC_TYPE_AIRKISS)); */
+  /* ESP_ERROR_CHECK(esp_smartconfig_set_type(SC_TYPE_ESPTOUCH_AIRKISS)); */
+  /* ESP_ERROR_CHECK(esp_smartconfig_set_type(SC_TYPE_ESPTOUCH_V2)); */
   smartconfig_start_config_t cfg = SMARTCONFIG_START_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK( esp_smartconfig_start(&cfg) );
+  ESP_ERROR_CHECK(esp_smartconfig_start(&cfg));
   while (1) {
-    uxBits = xEventGroupWaitBits(s_wifi_client_event_group, CONNECTED_BIT | ESPTOUCH_DONE_BIT, true, false, portMAX_DELAY);
+    uxBits = xEventGroupWaitBits(
+      s_wifi_client_event_group,
+      CONNECTED_BIT | ESPTOUCH_DONE_BIT, true, false, portMAX_DELAY);
     if(uxBits & CONNECTED_BIT) {
       ESP_LOGI(TAG, "WiFi Connected to ap");
     }
@@ -97,7 +109,13 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
       ESP_LOGI(TAG, "WIFI_EVENT: event_id = %d\n", event_id);
       return;
     }
-  } else if (event_base == IP_EVENT) {
+  }
+}
+
+static void ip_event_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data)
+{
+  if (event_base == IP_EVENT) {
     switch(event_id) {
     case IP_EVENT_STA_GOT_IP:
       xEventGroupSetBits(s_wifi_client_event_group, CONNECTED_BIT);
@@ -106,7 +124,13 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
       ESP_LOGI(TAG, "IP_EVENT: event_id = %d\n", event_id);
       return;
     }
-  } else if (event_base == SC_EVENT) {
+  }
+}
+
+static void smart_config_event_handler(void* arg, esp_event_base_t event_base,
+                               int32_t event_id, void* event_data)
+{
+  if (event_base == SC_EVENT) {
     switch(event_id) {
     case SC_EVENT_SCAN_DONE:
       ESP_LOGI(TAG, "Scan done");
@@ -120,8 +144,7 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
       wifi_config_t wifi_config;
       uint8_t ssid[33] = { 0 };
       uint8_t password[65] = { 0 };
-      uint8_t rvd_data[33] = { 0 };
-      bzero(&wifi_config, sizeof(wifi_config_t));
+      memset(&wifi_config, 0, sizeof(wifi_config_t));
       memcpy(wifi_config.sta.ssid, evt->ssid, sizeof(wifi_config.sta.ssid));
       memcpy(wifi_config.sta.password, evt->password, sizeof(wifi_config.sta.password));
       wifi_config.sta.bssid_set = evt->bssid_set;
@@ -133,16 +156,8 @@ static void wifi_event_handler(void* arg, esp_event_base_t event_base,
       memcpy(password, evt->password, sizeof(evt->password));
       ESP_LOGI(TAG, "SSID:%s", ssid);
       ESP_LOGI(TAG, "PASSWORD:%s", password);
-      if (evt->type == SC_TYPE_ESPTOUCH_V2) {
-        ESP_ERROR_CHECK( esp_smartconfig_get_rvd_data(rvd_data, sizeof(rvd_data)) );
-        ESP_LOGI(TAG, "RVD_DATA:");
-        for (int i=0; i<33; i++) {
-          printf("%02x ", rvd_data[i]);
-        }
-        printf("\n");
-      }
-      ESP_ERROR_CHECK( esp_wifi_disconnect() );
-      ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+      ESP_ERROR_CHECK(esp_wifi_disconnect());
+      ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
       esp_wifi_connect();
       break;
     case SC_EVENT_SEND_ACK_DONE:
