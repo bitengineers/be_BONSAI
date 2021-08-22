912145c9804593de4013cbd14dcb941ba4a930e3
jun fujita <716663+jfcamel@users.noreply.github.com>
2021-08-22 16:32:11 +0900

    wificlient implementation

--- iot_firmware/main/main.c
+++ iot_firmware/main/main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+#include "esp_log.h"
+
 #include "wificlient.h"
+
+static const char *TAG = "IoT_Plant";
 
 void app_main(void)
 {
+  ESP_LOGI(TAG, "app_main: started.");
 
+  wifi_client_init();
+  wifi_client_wait_for_connected(pdMS_TO_TICKS(1000 * 60 * 60 * 1));
+
+
+  while (1) {
+    vTaskDelay(pdMS_TO_TICKS(10 * 1000));
+  }
 }
