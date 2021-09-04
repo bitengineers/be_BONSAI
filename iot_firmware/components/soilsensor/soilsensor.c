1168578ce0eb0aa5f9a4d02dbdab7092dc629495
jun fujita <716663+jfcamel@users.noreply.github.com>
2021-09-04 22:00:45 +0900

    Disable digital pin on soilsensor

--- iot_firmware/components/soilsensor/soilsensor.c
+++ iot_firmware/components/soilsensor/soilsensor.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+
+#include "hal/gpio_hal.h"
+
 #include "soilsensor.h"
 #include "analogsensor.h"
 
@@ -12,21 +15,26 @@ static analogsensor_t s_soilsensor = {
 
 void soilsensor_init(void)
 {
-  gpio_config_t conf;
-  conf.mode = GPIO_MODE_OUTPUT;
-  conf.pin_bit_mask = 1ULL  << GROVE_DIGITAL_INPUT;
-  conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-  conf.pull_up_en = GPIO_PULLUP_DISABLE;
-  conf.intr_type = GPIO_INTR_DISABLE;
-  gpio_config(&conf);
-  // gpio_set_level((gpio_num_t)GROVE_DIGITAL_INPUT, 0);
-  gpio_set_level((gpio_num_t)GROVE_DIGITAL_INPUT, 1);
+  /*
+    gpio_config_t conf;
+    conf.mode = GPIO_MODE_OUTPUT;
+    conf.pin_bit_mask = 1ULL  << GROVE_DIGITAL_INPUT;
+    conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    conf.intr_type = GPIO_INTR_DISABLE;
+    ESP_ERROR_CHECK(gpio_config(&conf));
+    ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)GROVE_DIGITAL_INPUT, 0));
+    // ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)GROVE_DIGITAL_INPUT, 1));
+    */
   analogsensor_init(&s_soilsensor);
 }
 
 void soilsensor_deinit(void)
 {
-  gpio_set_level((gpio_num_t)GROVE_DIGITAL_INPUT, 0);
+  /*
+    ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)GROVE_DIGITAL_INPUT, 0));
+    ESP_ERROR_CHECK(gpio_reset_pin((gpio_num_t)GROVE_DIGITAL_INPUT));
+  */
   analogsensor_deinit(&s_soilsensor);
 }
 
