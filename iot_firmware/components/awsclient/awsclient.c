3d0e4d1972d4af8e047201178ce6cf48b00b7ee7
jun fujita <716663+jfcamel@users.noreply.github.com>
2021-09-05 13:59:01 +0900

    Updated awsclient

--- iot_firmware/components/awsclient/awsclient.c
+++ iot_firmware/components/awsclient/awsclient.c
@@ -1,7 +1,29 @@
 #include <stdio.h>
+
+#include "esp_log.h"
+
+#include "aws_iot_config.h"
+#include "aws_iot_mqtt_client_interface.h"
+#include "aws_iot_shadow_interface.h"
+
 #include "awsclient.h"
 
-void func(void)
-{
+#define TAG  "AWSCLIENT"
 
+static AWS_IoT_Client s_aws_mqtt_client;
+
+void awsclient_mqtt_init(awsclient_mqtt_config_t *config)
+{
+}
+
+void awsclient_mqtt_deinit(awsclient_mqtt_config_t *config)
+{
+}
+
+void awsclient_shadow_init(void)
+{
+}
+
+void awsclient_shadow_update(void)
+{
 }
--- iot_firmware/components/awsclient/include/awsclient.h
+++ iot_firmware/components/awsclient/include/awsclient.h
@@ -1,1 +1,9 @@
-void func(void);
+#pragma once
+
+typedef struct _awsclient_mqtt_config {
+} awsclient_mqtt_config_t;
+
+void awsclient_mqtt_init(awsclient_mqtt_config_t *config);
+void awsclient_mqtt_deinit(awsclient_mqtt_config_t *config);
+void awsclient_shadow_init(void);
+void awsclient_shadow_update(void);
