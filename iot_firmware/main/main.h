f193c8c89aa7442d9604aa7c82fa2420216651a9
jun fujita <716663+jfcamel@users.noreply.github.com>
2021-09-05 14:00:21 +0900

    Handling certs file for aws

---
+++ iot_firmware/main/main.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+
+extern const uint8_t aws_root_ca_pem_start[] asm("_binary_aws_root_ca_pem_start");
+extern const uint8_t aws_root_ca_pem_end[] asm("_binary_aws_root_ca_pem_end");
+extern const uint8_t certificate_pem_crt_start[] asm("_binary_certificate_pem_crt_start");
+extern const uint8_t certificate_pem_crt_end[] asm("_binary_certificate_pem_crt_end");
+extern const uint8_t private_pem_key_start[] asm("_binary_private_pem_key_start");
+extern const uint8_t private_pem_key_end[] asm("_binary_private_pem_key_end");
