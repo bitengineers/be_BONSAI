c4c9a2af7d67b4f2b7305182d32003397eb8cbf9
jun fujita <716663+jfcamel@users.noreply.github.com>
2021-10-09 22:15:51 +0900

    clean up

--- iot_firmware/components/soilsensor/soilsensor.c
+++ iot_firmware/components/soilsensor/soilsensor.c
@@ -15,26 +15,11 @@ static analogsensor_t s_soilsensor = {
 
 void soilsensor_init(void)
 {
-  /*
-    gpio_config_t conf;
-    conf.mode = GPIO_MODE_OUTPUT;
-    conf.pin_bit_mask = 1ULL  << GROVE_DIGITAL_INPUT;
-    conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    conf.pull_up_en = GPIO_PULLUP_DISABLE;
-    conf.intr_type = GPIO_INTR_DISABLE;
-    ESP_ERROR_CHECK(gpio_config(&conf));
-    ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)GROVE_DIGITAL_INPUT, 0));
-    // ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)GROVE_DIGITAL_INPUT, 1));
-    */
   analogsensor_init(&s_soilsensor);
 }
 
 void soilsensor_deinit(void)
 {
-  /*
-    ESP_ERROR_CHECK(gpio_set_level((gpio_num_t)GROVE_DIGITAL_INPUT, 0));
-    ESP_ERROR_CHECK(gpio_reset_pin((gpio_num_t)GROVE_DIGITAL_INPUT));
-  */
   analogsensor_deinit(&s_soilsensor);
 }
 
