60736bfcfcfa8f1cb53e19e22523570d89b3514a
jun fujita <716663+jfcamel@users.noreply.github.com>
2022-07-31 12:38:26 +0900

    Added test dir & test stub for wificlient

---
+++ iot_firmware/components/wificlient/test/wificlient_test.c
@@ -0,0 +1,13 @@
+#include <limits.h>
+#include "unity.h"
+#include "wificlient.h"
+
+TEST_CASE("wifi connection success scenario", "[success]")
+{
+  TEST_ASSERT_EQUAL(0, 0);
+}
+
+TEST_CASE("wifi connection failure scenario", "[failure]")
+{
+  TEST_ASSERT_EQUAL(0, 0);
+}
---
+++ iot_firmware/test/main/main_test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "unity.h"
+
+
+static void print_banner(const char* text);
+
+void app_main(void)
+{
+  print_banner("Running all the registered tests");
+  UNITY_BEGIN();
+  unity_run_all_tests();
+  UNITY_END();
+
+  print_banner("Starting interactive test menu");
+  unity_run_menu();
+}
+
+static void print_banner(const char* text)
+{
+    printf("\n#### %s #####\n\n", text);
+}
